2db1d7a608f04c813ff1d5524bd22a7cf4a1dab7
Darkhanova <91181096+Darkhanova@users.noreply.github.com>
2021-10-06 01:14:33 +0800

    Update Form2.cs

--- WindowsFormsApp1/WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -12,9 +12,34 @@ namespace WindowsFormsApp1
 {
     public partial class ifform : Form
     {
+        private string _path1 = @"C:\Users\ДНС\source\repos\WindowsFormsApp1\save1.txt";
+        private string _path2 = @"C:\Users\ДНС\source\repos\WindowsFormsApp1\save2.txt";
+        private string _path3 = @"C:\Users\ДНС\source\repos\WindowsFormsApp1\save3.txt";
+        private StreamReader sr;
         public ifform()
         {
             InitializeComponent();
+            sr = new StreamReader(_path1);
+            if (File.Exists(_path1))
+            {
+                string temp = sr.ReadLine();
+                sr.Close();
+                inA.Text = temp;
+            }
+            sr = new StreamReader(_path2);
+            if (File.Exists(_path2))
+            {
+                string temp = sr.ReadLine();
+                sr.Close();
+                inB.Text = temp;
+            }
+            sr = new StreamReader(_path3);
+            if (File.Exists(_path3))
+            {
+                string temp = sr.ReadLine();
+                sr.Close();
+                inC.Text = temp;
+            }
         }
 
         private void start1_Click(object sender, EventArgs e)
@@ -32,19 +57,66 @@ namespace WindowsFormsApp1
                 if (B < C)
                 {
                     res = A * B;
-                    MessageBox.Show("мин. произведение = " + res);
+                    label2.Text = Convert.ToString(res);
                 }
                 else
                 {
                     res = A * C;
-                    MessageBox.Show("мин. произведение = " + res);
+                    label2.Text = Convert.ToString(res);
                 }
             }
             else
             {
                 res = B * C;
-                MessageBox.Show("мин. произведение = " + res);
+                label2.Text = Convert.ToString(res);
             }
         }
+              private void ifform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+           
+        }
+
+        private void inA_TextChanged(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(_path1);
+            sw.WriteLine(inA.Text);
+            sw.Close();
+        }
+
+        private void inB_TextChanged(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(_path2);
+            sw.WriteLine(inB.Text);
+            sw.Close();
+        }
+
+        private void inC_TextChanged(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(_path3);
+            sw.WriteLine(inC.Text);
+            sw.Close();
+        }
     }
-}
+}  
+
+
